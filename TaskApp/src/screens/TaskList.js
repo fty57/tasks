bacc3ad738e5bd1b47dc0c64cf44acf02d459e5c
Faitly <fty.57@alu.ufc.br>
2021-11-13 13:50:19 -0300

    TaksList

--- TaskApp/src/screens/TaskList.js
+++ TaskApp/src/screens/TaskList.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {View, TextView} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
 
 export default class TaskList extends Component {
      render() {
