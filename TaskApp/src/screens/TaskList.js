b3f34d247a2667abe7b301d0b9cb3fec6bbd303e
Faitly <fty.57@alu.ufc.br>
2021-11-14 08:12:42 -0300

    Update TaskList.js

--- TaskApp/src/screens/TaskList.js
+++ TaskApp/src/screens/TaskList.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
      render() {
           return (
-               <View>
-                    <Text>TaskList</Text>
+               <View style={styles.container}>
+                    <ImageBackground
+                         source={todayImage}
+                         style={styles.background}>
+
+                    </ImageBackground>
+
+                    <View style={styles.taskList}>
+                         <Text>TaskList</Text>
+                    </View>
                </View>
           )
      }
 }
+
+
+const styles = StyleSheet.create({
+     container: {
+          flex: 1, // Você permite que esse container cresça, é a mesma coisa de flexGrow
+     },
+     background: {
+          flex: 3 // Como você dividisse sua tela em partes - 30%
+     },
+     taskList: {
+          flex: 7 // Cresce - 70%
+     }
+})
