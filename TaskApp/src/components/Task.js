5cb673467d1e55ba294fa3d3631ff125445f3a8a
Faitly <fty.57@alu.ufc.br>
2021-11-16 07:35:19 -0300

    Update Task.js

--- TaskApp/src/components/Task.js
+++ TaskApp/src/components/Task.js
@@ -1,13 +1,23 @@
 // Quando um componente não tem estado, mas é interessante você criar um funcional
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text , StyleSheet } from 'react-native'
 
 export default props =>{
      return(
-          <View>
+          <View style={styles.container}>
                <Text>{props.desc}</Text>
                <Text>{props.estimateAt + ''}</Text>
                <Text>{props.doneAt + ''}</Text>
           </View>
      )
 }
+
+const styles = StyleSheet.create({
+     container: {
+          flexDirection: 'row',
+          borderColor: '#AAA',
+          borderBottomWidth: 1,
+          alignItems: 'center',
+          paddingVertical: 10
+     }
+})
