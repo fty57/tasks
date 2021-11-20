d6b3c78bdec3fbc9ad74eef9f13eea9c593297a1
Faitly <fty.57@alu.ufc.br>
2021-11-20 13:24:06 -0300

    Update AddTask

--- TaskApp/src/screens/AddTask.js
+++ TaskApp/src/screens/AddTask.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text} from 'react-native'
+
+import commonStyles from '../commonStyles'
 
 export default class AddTask extends Component {
      render() {
@@ -7,6 +9,13 @@ export default class AddTask extends Component {
                <Modal transparent={true} visible={this.props.isVisible}
                     onRequestClose={this.props.onCancel}
                     animationType='slide'>
+                    <TouchableWithoutFeedback
+                         onPress={this.props.onCancel}>
+                         <View style={styles.background}>
+                              <Text style={styles.header}>Nova Tarefa</Text>
+                         </View>
+                    </TouchableWithoutFeedback>
+                    <View style={styles.container}></View>
                     <TouchableWithoutFeedback
                          onPress={this.props.onCancel}>
                          <View style={styles.background}>
@@ -22,5 +31,17 @@ const styles = StyleSheet.create({
      background: {
           flex: 1,
           backgroundColor: 'rgba(0, 0, 0, 0.7)'
+     },
+     container: {
+          flex: 1,
+          backgroundColor: '#FFF'
+     },
+     header:{
+          fontFamily: commonStyles.fontFamily,
+          backgroundColor: commonStyles.colors.today,
+          color: commonStyles.colors.secondary,
+          textAlign: 'center',
+          padding: 15,
+          fontSize: 18,
      }
 })
