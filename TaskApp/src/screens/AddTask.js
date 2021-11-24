b36e18375183ad98b0580b2c38884ba05fd774ff
Faitly <fty.57@alu.ufc.br>
2021-11-24 10:48:23 -0300

    Update AddTask.js

--- TaskApp/src/screens/AddTask.js
+++ TaskApp/src/screens/AddTask.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text} from 'react-native'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, Text, TouchableOpacity, TextInput } from 'react-native'
 
 import commonStyles from '../commonStyles'
 
+// Referências do estado inicial, caso seja preciso restartar
+const initialState = { desc: ''}
+
+// Uma vez que você coloca valor no textInput você nota que não consegue mais digitar
+// Não é o componente TextInput que altera o estado, é o estado que altera o componente
+// onChangeText={desc => this.setState({desc})} - Essa linha altera o estado
+// value={this.state.desc} - O valor do estado sendo alterado, ele será refletido na interface
+
 export default class AddTask extends Component {
+
+     state = {
+          ...initialState
+     }
+
      render() {
           return (
                <Modal transparent={true} visible={this.props.isVisible}
                     onRequestClose={this.props.onCancel}
                     animationType='slide'>
+
                     <TouchableWithoutFeedback
                          onPress={this.props.onCancel}>
                          <View style={styles.background}>
-                              <Text style={styles.header}>Nova Tarefa</Text>
+
                          </View>
                     </TouchableWithoutFeedback>
-                    <View style={styles.container}></View>
+
+                    <View style={styles.container}>
+
+                         <Text style={styles.header}>Nova Tarefa</Text>
+                         <TextInput 
+                         style={styles.input} 
+                              placeholder='Informe a Descrição...'
+                              onChangeText={desc => this.setState({desc})}
+                              value={this.state.desc}     
+                         />
+                         <View style={styles.buttons}>
+                              <TouchableOpacity onPress={this.props.onCancel}>
+                                   <Text style={styles.button}>Cancelar</Text>
+                              </TouchableOpacity>
+
+                              <TouchableOpacity>
+                                   <Text style={styles.button}>Salvar</Text>
+                              </TouchableOpacity>
+                         </View>
+                    </View>
+
                     <TouchableWithoutFeedback
                          onPress={this.props.onCancel}>
                          <View style={styles.background}>
@@ -33,15 +67,34 @@ const styles = StyleSheet.create({
           backgroundColor: 'rgba(0, 0, 0, 0.7)'
      },
      container: {
-          flex: 1,
           backgroundColor: '#FFF'
      },
-     header:{
+     header: {
           fontFamily: commonStyles.fontFamily,
           backgroundColor: commonStyles.colors.today,
           color: commonStyles.colors.secondary,
           textAlign: 'center',
           padding: 15,
           fontSize: 18,
+     },
+     input: {
+          fontFamily: commonStyles.fontFamily,
+          height: 40,
+          margin: 15,
+          backgroundColor: '#FFF',
+          borderWidth: 1,
+          borderColor: '#E3E3E3',
+          borderRadius: 6
+     },
+     buttons: {
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+
+     },
+     button:{
+          margin: 20,
+          marginRight: 30,
+          color: commonStyles.colors.today
      }
+
 })
