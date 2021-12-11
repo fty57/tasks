65e95b62d88b3e7b4d64ca46fe8c6af0cf6f966f
Faitly <fty.57@alu.ufc.br>
2021-12-11 12:29:23 -0300

    Update common.js

--- TaskApp/src/common.js
+++ TaskApp/src/common.js
@@ -4,7 +4,12 @@ const server = Plataform.OS === 'ios'
      ? 'https://localhost:3000' : 'http://10.0.2.2:3000'
 
 function showError(err) {
-     Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+     if(err.response && err.response.data){
+          Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err.response.data}`)
+     }else{
+          // Object.keys() assim você sabe exatamente as chaves que existem dentro 
+          Alert.alert('Ops! Ocorreu um Problema!', `Mensagem: ${err}`)
+     }
 }
 
 function showSuccess(msg) {
